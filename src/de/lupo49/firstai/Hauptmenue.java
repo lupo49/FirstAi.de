60fd5b908ff6427ea0aeb47c286530dd2280ccf0
Matthias Schulte <post@lupo49.de>
2011-06-23 12:19:04 +0200

    Effizienteres Handling der Menuepunkte / Layout-Umstellung / String-Ergaenzungen

--- src/de/lupo49/firstai/Hauptmenue.java
+++ src/de/lupo49/firstai/Hauptmenue.java
@@ -1,5 +1,7 @@
 package de.lupo49.firstai;
 
+import java.util.Vector;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,25 +14,36 @@ import android.widget.TextView;
 public class Hauptmenue extends Activity {
     
 	private Intent menuepunktAusgewaehlt;
+	private TextView menuepunkt;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        TextView menuepunkt = (TextView)findViewById(R.id.HaftungHinweise);
+        Vector<Integer> menuepunkte = new Vector<Integer>();
+        menuepunkte.add(R.id.HaftungHinweise);
+        menuepunkte.add(R.id.NotrufeWeltweit);
+        menuepunkte.add(R.id.Allgemeines);
+        menuepunkte.add(R.id.AllergischeReaktion);
+        menuepunkte.add(R.id.Amputation);
+        menuepunkte.add(R.id.AsthmaAtemnot);
+        menuepunkte.add(R.id.Atemstillstand);
+        
         menuepunktAusgewaehlt = new Intent(this, Unterpunkt.class);
         
-        
-        menuepunkt.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				menuepunktAusgewaehlt.putExtra("quelle", v.getId());
-				Log.i("Hauptmenue", String.valueOf(v.getId()));
-				startActivity(menuepunktAusgewaehlt);
-			}
-		});
-        
+        // Alle Menuepunkte eigene OnClickListener zuordnen, um die Detailseite aufzurufen
+        for(int i = 0; i < menuepunkte.size(); i++) {
+        	menuepunkt = (TextView)findViewById(menuepunkte.get(i));
+        	menuepunkt.setOnClickListener(new OnClickListener() {
+    			@Override
+    			public void onClick(View v) {
+    				menuepunktAusgewaehlt.putExtra("quelle", v.getId());
+    				Log.i("Hauptmenue", String.valueOf(v.getId()));
+    				startActivity(menuepunktAusgewaehlt);
+    			}
+    		});
+        }
     }
     
     @Override
--- src/de/lupo49/firstai/Unterpunkt.java
+++ src/de/lupo49/firstai/Unterpunkt.java
@@ -14,33 +14,55 @@ public class Unterpunkt extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.menuepunkt);
+		setContentView(R.layout.unterpunkt);
 		
 		int menuepunktID = -1;
 		CharSequence ausgabeText = "";
- 
+		CharSequence ueberschriftText = "";
+		
 		final Bundle parameter = getIntent().getExtras();
 		if(parameter != null) {
 			// ID des angeklickten TextViews sichern
 			menuepunktID = parameter.getInt("quelle");
 		}
 		
-		if(menuepunktID == R.id.HaftungHinweise) 	ausgabeText = Html.fromHtml("Haftung + Hinweise, -- Software --\nErste Hilfe auf dem Handy\nVersion: j3.0\n" +
-																	"Release: 2008-12-24\nLizenz: Creative Commons BY-NC-ND 3.0\nCopyright: Kai Kajus " +
-																	"Noack\n\nErste Hilfe Illustrationen © Med4Teens\n\nDieses Programm soll Informationen" +
-																	"zur Ersten Hilfe geben. Es stellt jedoch in keiner Weise einen Ersatz für einen Erste-Hilfe-Kurs" +
-																	"dar, sondern dient der Auffrischung des bereits erworbenen Wissens." +
-																	"\n\n-- Haftungsausschluss --\nBitte beachten Sie, dass ich keine Verantwortung für Konsequenzen, " +
-																	"die aus der Nutzung entstehen, übernehme.\nJEGLICHE HAFTUNG IST AUSGESCHLOSSEN!\nVERWENDUNG AUF EIGENE GEFAHR!" +
-																	"\n\nIn allen Notfällen suchen Sie bitte sofort professionelle Hilfe.\n\n-- Entwicklung des Projekts --\n" +
-																	"Das Programm soll vielsprachig werden. Freiwillige Übersetzer gesucht!!\n\nWeitere Informationen erhalten Sie im" +
-																	"Internet unter www.firstai.de oder schreiben Sie eine E-Mail an: info@firstai.de");
+		switch(menuepunktID) {
+			case R.id.HaftungHinweise:
+				ausgabeText = this.getString(R.string.HaftungHinweiseContent);
+				ueberschriftText = this.getString(R.string.HaftungHinweise);
+				break;
+			case R.id.Allgemeines:
+				ausgabeText = this.getString(R.string.AllgemeinesContent);
+				ueberschriftText = this.getString(R.string.Allgemeines);
+				break;
+			case R.id.AllergischeReaktion:
+				break;
+			case R.id.Amputation:
+				ausgabeText = this.getString(R.string.AmputationContent);
+				ueberschriftText = this.getString(R.string.Amputation);
+				break;
+			case R.id.AsthmaAtemnot:
+				ausgabeText = this.getString(R.string.AsthmaAtemnot);
+				ueberschriftText = this.getString(R.string.AsthmaAtemnotContent);
+				break;
+			case R.id.Atemstillstand:
+				ausgabeText = this.getString(R.string.Atemstillstand);
+				ueberschriftText = this.getString(R.string.AtemstillstandContent);
+				break;
+			default:
+				ausgabeText = Html.fromHtml("");
+				break;
+		}
 		
+		// Ueberschrift setzen
+		TextView tvUeberschrift = new TextView(this);
+		tvUeberschrift = (TextView)findViewById(R.id.Informationsleiste);
+		tvUeberschrift.setText(ueberschriftText);
+		
+		// Inhalt auf Activity setzen
 		TextView tv = new TextView(this);
+		tv = (TextView)findViewById(R.id.inhaltContainer);
 		tv.setText(ausgabeText);
-		tv.setTextSize(16);
-		LinearLayout v = (LinearLayout)findViewById(R.id.mpLinearLayout);
-		v.addView(tv);
 		
 		Log.i("Unterpunkt", String.valueOf(menuepunktID));
 	}
